8615984b8af13b978b88b2f699a534e95c8fdf34
Jonathan Politzki <91631593+jonathan-politzki@users.noreply.github.com>
2026-02-25 11:16:56 -0800

    skeleton

---
+++ src/adapters/__init__.py
@@ -0,0 +1,5 @@
+from src.adapters.procrustes import OrthogonalProcrustes
+from src.adapters.affine import LowRankAffine
+from src.adapters.residual_mlp import ResidualMLP
+
+__all__ = ["OrthogonalProcrustes", "LowRankAffine", "ResidualMLP"]
---
+++ src/clustering/__init__.py
@@ -0,0 +1,4 @@
+from src.clustering.kmeans import KMeansClustering
+from src.clustering.drift_aware import DriftAwareClustering
+
+__all__ = ["KMeansClustering", "DriftAwareClustering"]
